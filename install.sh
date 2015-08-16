53bdddfff48fb51bbeb1df85b1d1d1700c9763d9
becxer <becxer87@gmail.com>
2015-08-16 12:42:51 +0900

    install vimrc & profile

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
+sudo apt-get install ssh
+sudo apt-get install vim
+sudo apt-get install htop
+
+cp ~/.profile ~/.profile.bk
+cp ./data/profile ~/.profile
+echo "source .profile" >> ~/.bashrc 
+source ~/.profile
+
+cp ~/.vimrc ~/.vimrc.bk
+cp ./data/vimrc ~/.vimrc
+
 sudo apt-get install libncursesw5-dev
 sudo apt-get install byobu
-sudo apt-get install vim
-tar -jxvf ./vifm-0.7.7.tar.bz2
+
+tar -jxvf ./data/vifm-0.7.7.tar.bz2
 cd vifm-0.7.7
 sudo ./configure
 sudo make
