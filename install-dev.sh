a2841b4b41d38de5fe652b629b7bfa81ceb3b927
HP100 <becxer87@gmail.com>
2015-10-23 02:20:01 +0900

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -18,6 +18,13 @@ sudo apt-get install libncursesw5-dev
 sudo apt-get install byobu
 sudo pip install pydemon
 
+#install for vifm (newer version & past version is not working..)
+tar -jxvf ./data/vifm-0.7.7.tar.bz2
+cd vifm-0.7.7
+sudo ./configure
+sudo make
+sudo make install
+
 #install sci-learn
 wget -O- http://neuro.debian.net/lists/trusty.jp.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
 sudo apt-key adv --recv-keys --keyserver hkp://pgp.mit.edu:80 0xA5D32F012649A5A9
