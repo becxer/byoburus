56db66762595b54f90fa83f0fb78aa2650508039
becxer <whitewest87@gmail.com>
2015-10-22 23:18:29 +0900

    seperate install vim & dev

--- install-vim.sh
+++ install-vim.sh
@@ -1,27 +1,16 @@
 #apt-get setting 
-sudo apt-add-repository ppa:webupd8team/java
 sudo apt-get update
 
 #linux tools
-sudo apt-get install ssh
 sudo apt-get install vim
-sudo apt-get install htop
-sudo apt-get install iftop
-sudo apt-get install curl
 sudo apt-get install exuberant-ctags
 
-#install language for default
-sudo apt-get install oracle-java8-installer
+#install library for default
 sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 sudo apt-get install build-essential python-dev python-setuptools libatlas-dev libatlas3gf-base
 sudo apt-get install python-pip
 
-#install byobu
-sudo apt-get install libncursesw5-dev
-sudo apt-get install byobu
-
 #install through pip
-sudo pip install pydemon
 sudo pip install jedi
 
 #replace vimrc
@@ -47,15 +36,6 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/aperezdc/vim-template.git
 git clone --recursive https://github.com/davidhalter/jedi-vim.git
-
-#install sci-learn
-wget -O- http://neuro.debian.net/lists/trusty.jp.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pgp.mit.edu:80 0xA5D32F012649A5A9
-sudo apt-get update
-sudo apt-get install python-sklearn
-sudo update-alternatives --set libblas.so.3 /usr/lib/atlas-base/atlas/libblas.so.3
-sudo update-alternatives --set liblapack.so.3 /usr/lib/atlas-base/atlas/liblapack.so.3
-nosetests -v sklearn
 
 echo "..."
 echo "=================================================="
