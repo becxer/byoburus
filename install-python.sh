2f27e01d7699cda46a173a3d79440c2c7f3a1b74
HP100 <becxer87@gmail.com>
2016-05-18 00:53:26 +0900

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -1,6 +1,3 @@
-#apt-get setting 
-sudo apt-get update
-
 #install language for default
 sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 sudo apt-get install build-essential python-dev python-setuptools libatlas-dev libatlas3gf-base
