c4ef1793f4125b372dfbb6093eac88b06e9af3b2
HP100 <becxer87@gmail.com>
2016-05-18 00:54:00 +0900

    Update install-vimrc.sh

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,8 +1,3 @@
-#install git required to git prompt
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install git
-
 git clone https://github.com/becxer/vimrc.git
 cd vimrc
 ./install-vim.sh
