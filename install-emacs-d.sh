9617abbc2a9b47f2b3c1e361ddc378c40eda54aa
becxer <becxer87@gmail.com>
2017-07-31 22:39:33 +0900

    add install ispell in install emacs

--- install-emacs-d.sh
+++ install-emacs-d.sh
@@ -4,4 +4,5 @@ rm -rf ~/.emacs.d
 rm -f ~/.emacs
 
 git clone https://github.com/becxer/.emacs.d.git ~/.emacs.d
-
+sudo apt-get install ispell
+sudo ln -s /usr/lib/ispell/american.hash /usr/lib/ispell/english.hash
