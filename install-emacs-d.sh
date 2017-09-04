24211bd5ad61364821d65da54ed2c07c1220355d
HP100 <becxer87@gmail.com>
2017-09-04 22:50:43 +0900

    Update install-emacs-d.sh

--- install-emacs-d.sh
+++ install-emacs-d.sh
@@ -5,5 +5,6 @@ rm -f ~/.emacs
 
 git clone https://github.com/becxer/.emacs.d.git ~/.emacs.d
 sudo apt-get install ispell
+sudo apt-get install fonts-hack-ttf
 sudo ln -s /usr/lib/ispell/american.hash /usr/lib/ispell/english.hash
 sudo ln -s /usr/bin/ispell /usr/local/bin/ispell
