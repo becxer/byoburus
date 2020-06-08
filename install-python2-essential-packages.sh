2805a289856b761977840d544c3bb0c30525fce6
Seohyun Back <becxer87@gmail.com>
2020-06-08 14:58:42 +0900

    Delete install-python2-essential-packages.sh

--- install-python2-essential-packages.sh
+++
@@ -1,8 +0,0 @@
-#install language for default
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
-sudo apt-get install build-essential python-dev python-setuptools libatlas-dev libatlas3gf-base
-sudo apt-get install python-pip
-
-sudo pip install virtualenv
-sudo pip install jedi
-sudo pip install epc
